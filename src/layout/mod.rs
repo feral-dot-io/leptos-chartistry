2bab4267ac9ae28f7157000849b12f9e6dd48924
Joshua McQuistan <joshua.mcquistan@feral.io>
2023-11-28 15:04:35 +0000

    Remove compose.rs

--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -1,9 +1,7 @@
-//mod compose;
 mod layout;
 pub mod legend;
 pub mod rotated_label;
 pub mod snippet;
 pub mod tick_labels;
 
-//pub use compose::{HorizontalLayout, UnconstrainedLayout, VerticalLayout};
 pub use layout::{HorizontalLayout, Layout, VerticalLayout};
