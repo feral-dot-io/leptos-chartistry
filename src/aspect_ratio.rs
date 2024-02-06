6944237853581d8b59637ab8e9a267901a39505d
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-02-06 23:44:40 +0000

    Expand aspect ratio docs

--- src/aspect_ratio.rs
+++ src/aspect_ratio.rs
@@ -1,8 +1,33 @@
 use leptos::*;
 
-/// Calculates width and height for a chart.
+/// Calculates the width and height of a chart.
 ///
-/// TODO greatly expand the documentation.
+/// An AspectRatio is built from the available constructors: `[inner|outer|environment]_[width|height|ratio]`
+///
+/// The first part `[inner|outer|environment]` is a choice of what kind of dimensions are we calculating:
+/// 1) the inner chart area,
+/// 2) the outer chart including the edge layout,
+/// 3) or from the environment using parent container dimensions (and then using them as an outer chart).
+///
+/// The second part `[width|height|ratio]` is a choice of which variable to calculate from the formula: `width / height = ratio`.
+///
+/// ## Why is this important?
+///
+/// TODO sunspot examples
+///
+/// ## Practical advice
+///
+/// Bank to 45 degrees. [0]
+///
+/// Bank the slopes of your lines to an average of 45 degrees and you'll maximise the ability to differentiate slope differences. Sunspots become readable. This is intended as a good heuristic to start with and then adjust as needed.
+///
+/// You should preference "inner" dimensions when you can which leads to a chart-centric approach (the chart is always matches your intention). However charts also live on a web page with constraints -- and its in this context that you'll probably prefer to work with a chart with known "outer" dimensions. When using outer dimensions try to ensure the layout is a fixed size regardless of the input data and this will be just as good as having a fixed inner chart (setting [TickLabels::min_chars](crate::TickLabels::min_chars) on the Y axis is an example of this).
+///
+/// Finally the "environment" dimensions work great for automatically grabbing the width or height from the parent container. The same caveats as "outer" apply but you have less control. YOLO! The risk is that if you change the page and incidentally change the dimensions, you'll change your perception of the chart.
+///
+/// I find that there's usually an obvious choice of width or height to pick from e.g., my page is growing horizontally or I'm expecting a lot of free space to the side. In these cases, I'll pick that variable and use a ratio that fits well for the chart. You can't really go wrong here as they're all part of the same choice.
+///
+/// [0] Cleveland, W. S., McGill, M. E., & McGill, R. (1988). The Shape Parameter of a Two-Variable Graph. Journal of the American Statistical Association, 83(402), 289–300. <https://doi.org/10.2307/2288843>
 #[derive(Clone, Debug, PartialEq)]
 pub struct AspectRatio(CalcUsing);
 
@@ -41,42 +66,42 @@ impl AspectRatio {
         Self(CalcUsing::Known(KnownAspectRatio::Inner(vars)))
     }
 
-    /// The height is set by width / ratio.
+    /// The outer height is set by width / ratio.
     pub const fn outer_height(width: f64, ratio: f64) -> Self {
         Self::new_outer(AspectRatioVars::WidthAndRatio(width, ratio))
     }
 
-    /// The width is set by height * ratio.
+    /// The outer width is set by height * ratio.
     pub const fn outer_width(height: f64, ratio: f64) -> Self {
         Self::new_outer(AspectRatioVars::HeightAndRatio(height, ratio))
     }
 
-    /// Sets the width and height of the chart. Ratio is implied width / height.
+    /// Sets the outer width and height of the chart. Ratio is implied width / height.
     pub const fn outer_ratio(width: f64, height: f64) -> Self {
         Self::new_outer(AspectRatioVars::WidthAndHeight(width, height))
     }
 
-    /// The height is set by width / ratio.
+    /// The inner height is set by the given width / ratio.
     pub const fn inner_height(width: f64, ratio: f64) -> Self {
         Self::new_inner(AspectRatioVars::WidthAndRatio(width, ratio))
     }
 
-    /// The width is set by height * ratio.
+    /// The inner width is set by the given height * ratio.
     pub const fn inner_width(height: f64, ratio: f64) -> Self {
         Self::new_inner(AspectRatioVars::HeightAndRatio(height, ratio))
     }
 
-    /// Sets the width and height of the chart. Ratio is implied width / height.
+    /// Sets the width and height of the inner chart. Ratio is implied width / height.
     pub const fn inner_ratio(width: f64, height: f64) -> Self {
         Self::new_inner(AspectRatioVars::WidthAndHeight(width, height))
     }
 
-    /// The outer height is set by the width of the parent container and a given ratio (width / ratio).
+    /// The outer height is set by the width of the parent container and the given outer ratio (width / ratio).
     pub const fn environment_height(ratio: f64) -> Self {
         Self(CalcUsing::Env(EnvCalc::WidthAndRatio(ratio)))
     }
 
-    /// The outer width is set by the height of the parent container and a given ratio (height * ratio).
+    /// The outer width is set by the height of the parent container and the given outer ratio (height * ratio).
     pub const fn environment_width(ratio: f64) -> Self {
         Self(CalcUsing::Env(EnvCalc::HeightAndRatio(ratio)))
     }
