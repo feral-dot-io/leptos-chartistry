ef7a875d4790dcf4655824e071e82747b483eedb
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-02-07 12:59:05 +0000

    Add menu to demo

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -112,26 +112,28 @@ pub fn f64_to_dt(at: f64) -> DateTime<Local> {
 pub fn App() -> impl IntoView {
     view! {
         <header>
-            <h2>
-                <a href="https://github.com/feral-dot-io/leptos-chartistry">
-                    "leptos-chartistry"
-                </a>
-            </h2>
-            <p>
-                <a href="https://github.com/feral-dot-io/leptos-chartistry">
-                    <img src="https://img.shields.io/badge/github-blue?logo=github&style=for-the-badge" alt="GitHub" />
-                </a>
-            </p>
-            <p>
-                <a href="https://crates.io/crates/leptos-chartistry">
-                    <img src="https://img.shields.io/crates/v/leptos-chartistry.svg?style=for-the-badge" alt="Crates.io version" />
-                </a>
-            </p>
-            <p>
-                <a href="https://docs.rs/leptos-chartistry">
-                <img src="https://img.shields.io/badge/docs-latest-blue.svg?style=for-the-badge" alt="Docs.rs" />
-                </a>
-            </p>
+            <h2><a href="https://github.com/feral-dot-io/leptos-chartistry">"leptos-chartistry"</a></h2>
+            <nav>
+                <p><a href="/">"Demo"</a></p>
+                <p><a href="/examples">"Examples"</a></p>
+            </nav>
+            <div class="badges">
+                <p>
+                    <a href="https://github.com/feral-dot-io/leptos-chartistry">
+                        <img src="https://img.shields.io/badge/github-blue?logo=github&style=for-the-badge" alt="GitHub" height="28px" />
+                    </a>
+                </p>
+                <p>
+                    <a href="https://crates.io/crates/leptos-chartistry">
+                        <img src="https://img.shields.io/crates/v/leptos-chartistry.svg?style=for-the-badge" alt="Crates.io version" height="28px" />
+                    </a>
+                </p>
+                <p>
+                    <a href="https://docs.rs/leptos-chartistry">
+                        <img src="https://img.shields.io/badge/docs-latest-blue.svg?style=for-the-badge" alt="Docs.rs" height="28px" />
+                    </a>
+                </p>
+            </div>
         </header>
 
         <main>
