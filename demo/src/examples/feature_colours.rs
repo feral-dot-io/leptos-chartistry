e1acc5f30c446d0c78f0d64fe4dfa668a8b06c1e
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-02-08 16:28:55 +0000

    Vary colour legend

--- demo/src/examples/feature_colours.rs
+++ demo/src/examples/feature_colours.rs
@@ -9,25 +9,25 @@ pub fn Example(debug: Signal<bool>, data: Signal<Vec<MyData>>) -> impl IntoView 
     const THISTLE: Colour = Colour::new(216, 191, 216);
     let sea_green: Colour = "#20b2aa".parse().unwrap();
     const RED: Colour = Colour::new(255, 0, 0);
-    const BLUE: Colour = Colour::new(0, 0, 255);
+    const BLUE_VIOLET: Colour = Colour::new(0, 0, 255);
 
     // We can also describe a colour scheme for our Series:
     // For non-stacked, colours are picked one after the other and then repeat
     // For stacked lines, colours are interpolated between the first and last
-    let scheme = ColourScheme::new(RED, vec![BLUE]);
+    let scheme = ColourScheme::new(RED, vec![BLUE_VIOLET]);
 
     // Add names to our lines for the legend to use
     let series = Series::new(|data: &MyData| data.x)
         .line(
             Line::new(|data: &MyData| data.y1)
-                .with_name("tea")
+                .with_name("roses")
                 // Manually specify the colour of a line
                 .with_colour(RED),
         )
         .line(
             Line::new(|data: &MyData| data.y2)
-                .with_name("coffee")
-                .with_colour(BLUE),
+                .with_name("violets")
+                .with_colour(BLUE_VIOLET),
         )
         // Or specify the colour scheme (this gives the same as above but more flexible)
         .with_colours(scheme);
