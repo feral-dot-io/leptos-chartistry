aeca36a7c9631c70220eb6fdbee011dcf080b8e2
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-02-10 23:48:07 +0000

    Add base URL to demo Routes

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -14,7 +14,7 @@ pub fn App() -> impl IntoView {
         <Router>
             <SiteHeader />
             <main>
-                <Routes>
+                <Routes base="/leptos-chartistry".to_string()>
                     <Route path="/" view=Demo />
                     <Route path="/examples" view=Examples />
                     <Route path="/*any" view=NotFound />
@@ -28,24 +28,24 @@ pub fn App() -> impl IntoView {
 fn SiteHeader() -> impl IntoView {
     view! {
         <header>
-            <h2><a href="https://github.com/feral-dot-io/leptos-chartistry">"leptos-chartistry"</a></h2>
+            <h2><a rel="external" href="https://github.com/feral-dot-io/leptos-chartistry">"leptos-chartistry"</a></h2>
             <nav>
-                <p><a href="/">"Demo"</a></p>
-                <p><a href="/examples">"Examples"</a></p>
+                <p><A href="/leptos-chartistry/">"Demo"</A></p>
+                <p><A href="/leptos-chartistry/examples">"Examples"</A></p>
             </nav>
             <div class="badges">
                 <p>
-                    <a href="https://github.com/feral-dot-io/leptos-chartistry">
+                    <a rel="external" href="https://github.com/feral-dot-io/leptos-chartistry">
                         <img src="https://img.shields.io/badge/github-blue?logo=github&style=for-the-badge" alt="GitHub" height="28px" />
                     </a>
                 </p>
                 <p>
-                    <a href="https://crates.io/crates/leptos-chartistry">
+                    <a rel="external" href="https://crates.io/crates/leptos-chartistry">
                         <img src="https://img.shields.io/crates/v/leptos-chartistry.svg?style=for-the-badge" alt="Crates.io version" height="28px" />
                     </a>
                 </p>
                 <p>
-                    <a href="https://docs.rs/leptos-chartistry">
+                    <a rel="external" href="https://docs.rs/leptos-chartistry">
                         <img src="https://img.shields.io/badge/docs-latest-blue.svg?style=for-the-badge" alt="Docs.rs" height="28px" />
                     </a>
                 </p>
