fe3bf6ae43385a268a1dc6cc9cbab4419d99b7ec
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-02-11 00:08:38 +0000

    Update demo site

--- docs/demo-9b0182a04351fbb4.js
+++ docs/demo-9b0182a04351fbb4.js
@@ -1322,7 +1322,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('demo-d0f11e69a23b388c_bg.wasm', import.meta.url);
+        input = new URL('demo-9b0182a04351fbb4_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
