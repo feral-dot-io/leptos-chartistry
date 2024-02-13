a90e9df85529a384a7f02968435e6478dbc18a52
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-02-13 22:19:17 +0000

    Add markers to legend snippets

--- leptos-chartistry/src/series/line/mod.rs
+++ leptos-chartistry/src/series/line/mod.rs
@@ -140,7 +140,7 @@ impl<T, Y> IntoUseLine<T, Y> for Line<T, Y> {
 }
 
 impl UseLine {
-    pub fn taster_bounds(font_height: Memo<f64>, font_width: Memo<f64>) -> Memo<Bounds> {
+    fn taster_bounds(font_height: Memo<f64>, font_width: Memo<f64>) -> Memo<Bounds> {
         create_memo(move |_| Bounds::new(font_width.get() * 2.0, font_height.get()))
     }
 
@@ -149,32 +149,17 @@ impl UseLine {
         Signal::derive(move || taster_bounds.get().width() + font_width.get())
     }
 
-    pub fn taster(&self, bounds: Memo<Bounds>) -> View {
-        view!( <LineTaster line=self.clone() bounds=bounds /> )
-    }
-
     pub(crate) fn render(&self, positions: Signal<Vec<(f64, f64)>>) -> View {
-        view!( <RenderLine line=self.clone() positions=positions /> )
+        view!( <RenderLine line=self.clone() positions=positions markers=positions /> )
     }
 }
 
 #[component]
-fn LineTaster(line: UseLine, bounds: Memo<Bounds>) -> impl IntoView {
-    let colour = line.colour;
-    view! {
-        <line
-            x1=move || bounds.get().left_x()
-            x2=move || bounds.get().right_x()
-            y1=move || bounds.get().centre_y() + 1.0
-            y2=move || bounds.get().centre_y() + 1.0
-            stroke=move || colour.get().to_string()
-            stroke-width=line.width
-        />
-    }
-}
-
-#[component]
-pub fn RenderLine(line: UseLine, positions: Signal<Vec<(f64, f64)>>) -> impl IntoView {
+pub fn RenderLine(
+    line: UseLine,
+    positions: Signal<Vec<(f64, f64)>>,
+    markers: Signal<Vec<(f64, f64)>>,
+) -> impl IntoView {
     let path = move || {
         positions.with(|positions| {
             let mut need_move = true;
@@ -206,7 +191,7 @@ pub fn RenderLine(line: UseLine, positions: Signal<Vec<(f64, f64)>>) -> impl Int
                 stroke=colour
                 stroke-width=width
             />
-            <marker::LineMarkers line=line positions=positions />
+            <marker::LineMarkers line=line positions=markers />
         </g>
     }
 }
@@ -225,20 +210,32 @@ pub fn Snippet<X: 'static, Y: 'static>(series: UseLine, state: State<X, Y>) -> i
 }
 
 #[component]
-pub fn Taster<X: 'static, Y: 'static>(series: UseLine, state: State<X, Y>) -> impl IntoView {
+fn Taster<X: 'static, Y: 'static>(series: UseLine, state: State<X, Y>) -> impl IntoView {
+    const Y_OFFSET: f64 = 2.0;
     let debug = state.pre.debug;
     let font_width = state.pre.font_width;
     let bounds = UseLine::taster_bounds(state.pre.font_height, font_width);
+    // Mock positions from left to right of our bounds
+    let positions = Signal::derive(move || {
+        let bounds = bounds.get();
+        let y = bounds.centre_y() + Y_OFFSET;
+        vec![(bounds.left_x(), y), (bounds.right_x(), y)]
+    });
+    // One marker in the middle
+    let markers = Signal::derive(move || {
+        let bounds = bounds.get();
+        vec![(bounds.centre_x(), bounds.centre_y() + Y_OFFSET)]
+    });
     view! {
         <svg
-            class="_chartistry_taster"
+            viewBox=move || format!("0 0 {} {}", bounds.get().width(), bounds.get().height())
             width=move || bounds.get().width() + font_width.get()
             height=move || bounds.get().height()
-            viewBox=move || format!("0 0 {} {}", bounds.get().width(), bounds.get().height())
+            class="_chartistry_taster"
             style="box-sizing: border-box;"
             style:padding-right=move || format!("{}px", font_width.get())>
             <DebugRect label="taster" debug=debug bounds=vec![bounds.into()] />
-            {series.taster(bounds)}
+            <RenderLine line=series positions=positions markers=markers />
         </svg>
     }
 }
