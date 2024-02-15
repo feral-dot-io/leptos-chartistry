91fd5319fa3d6aa6b7b41b8029925f7de8af7325
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-02-15 11:23:37 +0000

    Add diverging gradient example

--- demo/src/examples/feature_line_gradient.rs
+++ demo/src/examples/feature_line_gradient.rs
@@ -2,18 +2,40 @@ use super::MyData;
 use leptos::*;
 use leptos_chartistry::*;
 
+// Move Y so that values cross zero to demonstrate a diverging gradient
+const Y_OFFSET: f64 = -4.0;
+
 #[component]
 pub fn Example(debug: Signal<bool>, data: Signal<Vec<MyData>>) -> impl IntoView {
-    let gradient: ColourScheme = LINEAR_GRADIENT.into();
     let series = Series::new(|data: &MyData| data.x)
-        .line(Line::new(|data: &MyData| data.y1).with_gradient(LINEAR_GRADIENT))
-        .line(Line::new(|data: &MyData| data.y2).with_gradient(gradient.invert()));
+        .line(
+            Line::new(|data: &MyData| data.y1 + Y_OFFSET)
+                .with_width(5.0)
+                // Add a linear gradient that changes the line colour based on
+                // the Y value.
+                .with_gradient(LINEAR_GRADIENT),
+        )
+        .line(
+            Line::new(|data: &MyData| data.y2 + Y_OFFSET)
+                .with_width(5.0)
+                // Add a diverging gradient that also changes the line colour
+                // based on the Y value except it has a central value where the
+                // gradient flips. In our case we show blue below zero and red
+                // above.
+                .with_gradient(ColourScheme::diverging_gradient(
+                    // TODO: use into???
+                    DIVERGING_GRADIENT.0.into(),
+                    DIVERGING_GRADIENT.1.into(),
+                )),
+        );
     view! {
         <Chart
             aspect_ratio=AspectRatio::from_outer_height(300.0, 1.2)
             debug=debug
             series=series
             data=data
+            // Show the zero line -- where a diverging gradient would flip
+            inner=AxisMarker::horizontal_zero()
         />
     }
 }
