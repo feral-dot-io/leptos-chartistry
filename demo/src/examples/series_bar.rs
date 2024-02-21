d6187d35b36e6a6f032017d71c464edadb9e5139
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-02-21 15:21:52 +0000

    Add names to bar example. Remove grid lines

--- demo/src/examples/series_bar.rs
+++ demo/src/examples/series_bar.rs
@@ -6,8 +6,8 @@ use leptos_chartistry::*;
 pub fn Example(debug: Signal<bool>, data: Signal<Vec<MyData>>) -> impl IntoView {
     // Lines are added to the series
     let series = Series::new(|data: &MyData| data.x)
-        .bar(Bar::new(|data: &MyData| data.y1))
-        .bar(Bar::new(|data: &MyData| data.y2));
+        .bar(Bar::new(|data: &MyData| data.y1).with_name("trees"))
+        .bar(Bar::new(|data: &MyData| data.y2).with_name("plants"));
     view! {
         <Chart
             aspect_ratio=AspectRatio::from_outer_height(300.0, 1.2)
@@ -20,11 +20,8 @@ pub fn Example(debug: Signal<bool>, data: Signal<Vec<MyData>>) -> impl IntoView 
             left=TickLabels::aligned_floats()
             bottom=Legend::end()
             inner=[
-                // Standard set of inner layout options
                 AxisMarker::left_edge().into_inner(),
                 AxisMarker::bottom_edge().into_inner(),
-                XGridLine::default().into_inner(),
-                YGridLine::default().into_inner(),
                 YGuideLine::over_mouse().into_inner(),
                 XGuideLine::over_data().into_inner(),
             ]
