da114f438c93aaa15dadcf24f951bda69a3e4c30
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-02-22 15:25:31 +0000

    Remove duplciate BarExample

--- demo/src/pages/examples.rs
+++ demo/src/pages/examples.rs
@@ -53,15 +53,6 @@ example!(
     "../examples/series_bar.rs"
 );
 
-// Bars
-example!(
-    BarExample,
-    series_bar::Example,
-    "Bar chart",
-    "A simple bar chart.",
-    "../examples/series_bar.rs"
-);
-
 // Edge layout options
 example!(
     LegendExample,
