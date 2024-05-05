91560a9eb4992f8d7523b7a7beb91ca81c5feafd
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-05-05 21:51:40 +0100

    Remove pages/examples sub mods

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -2,11 +2,5 @@ pub mod data;
 pub mod examples;
 pub mod pages {
     pub mod demo;
-    pub mod examples {
-        mod all_examples;
-        pub use all_examples::*;
-
-        // Individual examples
-        //pub mod series_line;
-    }
+    pub mod examples;
 }
