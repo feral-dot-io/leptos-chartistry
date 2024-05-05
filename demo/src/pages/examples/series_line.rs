25102c72bd7d4057a10ade9ec17051472bbadea4
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-05-05 19:21:35 +0100

    Show code in example

--- demo/src/pages/examples/series_line.rs
+++ demo/src/pages/examples/series_line.rs
@@ -5,9 +5,17 @@ use leptos::*;
 pub fn Example() -> impl IntoView {
     let debug = create_rw_signal(false);
     let data = load_data();
+    let code = include_str!("../../examples/series_line.rs");
     view! {
-        <article id="examples">
-            <series_line::Example debug=debug.into() data=data />
+        <article id="example-line-chart" class="example">
+            <div class="background-box demo">
+                <h1 class="connect-heading">"Line chart"</h1>
+                <series_line::Example debug=debug.into() data=data />
+            </div>
+            <div class="background-box code">
+                <h2 class="connect-heading">"Example code"</h2>
+                <pre><code>{code}</code></pre>
+            </div>
         </article>
     }
 }
