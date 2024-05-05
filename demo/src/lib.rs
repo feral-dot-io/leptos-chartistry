b1eb836dcb5ed0106e6c805fc352db2da1f6453f
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-05-05 18:35:09 +0100

    Move pages/examples/mod.rs into all_examples to allow non-mod.rs expansion of sub mods

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -2,5 +2,8 @@ pub mod data;
 pub mod examples;
 pub mod pages {
     pub mod demo;
-    pub mod examples;
+    pub mod examples {
+        mod all_examples;
+        pub use all_examples::*;
+    }
 }
