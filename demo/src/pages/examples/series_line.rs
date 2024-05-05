bee164765ea7f4943aeb9813503f8603645458bf
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-05-05 19:54:18 +0100

    Add debug button to example page

--- demo/src/pages/examples/series_line.rs
+++ demo/src/pages/examples/series_line.rs
@@ -8,9 +8,19 @@ pub fn Example() -> impl IntoView {
     let code = include_str!("../../examples/series_line.rs");
     view! {
         <article id="example-line-chart" class="example">
-            <div class="background-box demo">
-                <h1 class="connect-heading">"Line chart"</h1>
-                <series_line::Example debug=debug.into() data=data />
+            <div class="cards">
+                <div class="background-box demo">
+                    <h1 class="connect-heading">"Line chart"</h1>
+                    <p>"A simple line chart."</p>
+                    <series_line::Example debug=debug.into() data=data />
+                </div>
+                <p class="background-box debug">
+                    <label>
+                        <input type="checkbox" input type="checkbox"
+                            on:input=move |ev| debug.set(event_target_checked(&ev)) />
+                        " Toggle debug mode"
+                    </label>
+                </p>
             </div>
             <div class="background-box code">
                 <h2 class="connect-heading">"Example code"</h2>
