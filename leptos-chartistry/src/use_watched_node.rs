0f7d5939dbe5b44331b81254960ba6a5430841a8
Francis Tseng <f+accounts@frnsys.com>
2024-08-14 20:23:35 +0200

    updated leptos-use to 0.12

--- leptos-chartistry/src/use_watched_node.rs
+++ leptos-chartistry/src/use_watched_node.rs
@@ -1,8 +1,15 @@
+use std::convert::Infallible;
+
 use crate::bounds::Bounds;
 use leptos::{html::Div, *};
 use leptos_use::{
-    use_element_hover, use_mouse_with_options, use_resize_observer_with_options, UseMouseCoordType,
-    UseMouseEventExtractorDefault, UseMouseOptions, UseMouseSourceType, UseResizeObserverOptions,
+    use_element_hover,
+    use_mouse_with_options,
+    use_resize_observer_with_options,
+    UseMouseCoordType,
+    UseMouseOptions,
+    UseMouseSourceType,
+    UseResizeObserverOptions,
 };
 use web_sys::ResizeObserverBoxOptions;
 
@@ -18,11 +25,13 @@ pub fn use_watched_node(node: NodeRef<Div>) -> UseWatchedNode {
     // Outer chart bounds -- dimensions for our root element inside the document
     // Note <svg> has issues around observing size changes. So wrap in a <div>
     // Note also that the box_ option doesn't seem to work for us so wrap in another <div>
-    let (bounds, set_bounds) = create_signal::<Option<Bounds>>(None);
+    let (bounds, set_bounds) =
+        create_signal::<Option<Bounds>>(None);
     use_resize_observer_with_options(
         node,
         move |entries, _| {
-            let rect = &entries[0].target().get_bounding_client_rect();
+            let rect =
+                &entries[0].target().get_bounding_client_rect();
             let rect = Bounds::new(rect.width(), rect.height());
             set_bounds.set(Some(rect))
         },
@@ -36,7 +45,7 @@ pub fn use_watched_node(node: NodeRef<Div>) -> UseWatchedNode {
     let mouse_page = use_mouse_with_options(
         UseMouseOptions::default()
             .target(node)
-            .coord_type(UseMouseCoordType::<UseMouseEventExtractorDefault>::Page)
+            .coord_type(UseMouseCoordType::<Infallible>::Page)
             .reset_on_touch_ends(true),
     );
 
@@ -52,7 +61,7 @@ pub fn use_watched_node(node: NodeRef<Div>) -> UseWatchedNode {
     let mouse_client = use_mouse_with_options(
         UseMouseOptions::default()
             .target(node)
-            .coord_type(UseMouseCoordType::<UseMouseEventExtractorDefault>::Client)
+            .coord_type(UseMouseCoordType::<Infallible>::Client)
             .reset_on_touch_ends(true),
     );
     let mouse_chart: Signal<_> = create_memo(move |_| {
@@ -92,8 +101,12 @@ pub fn use_watched_node(node: NodeRef<Div>) -> UseWatchedNode {
 
 impl UseWatchedNode {
     // Mouse inside inner chart?
-    pub fn mouse_hover_inner(&self, inner: Memo<Bounds>) -> Signal<bool> {
-        let (mouse_rel, hover) = (self.mouse_chart, self.mouse_chart_hover);
+    pub fn mouse_hover_inner(
+        &self,
+        inner: Memo<Bounds>,
+    ) -> Signal<bool> {
+        let (mouse_rel, hover) =
+            (self.mouse_chart, self.mouse_chart_hover);
         create_memo(move |_| {
             let (x, y) = mouse_rel.get();
             hover.get() && inner.get().contains(x, y)
