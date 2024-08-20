f34169dafe09867cbe07055807600f4730d7be33
Joshua McQuistan <joshua.mcquistan@feral.io>
2024-08-20 10:07:18 +0100

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -26,11 +26,19 @@
   };
 
   outputs =
-    { self, nixpkgs, utils, rust-overlay, crane, advisory-db, ... }@inputs:
-    utils.lib.eachDefaultSystem (system:
+    {
+      self,
+      nixpkgs,
+      utils,
+      rust-overlay,
+      crane,
+      advisory-db,
+      ...
+    }@inputs:
+    utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs =
-          nixpkgs.legacyPackages.${system}.extend rust-overlay.overlays.default;
+        pkgs = nixpkgs.legacyPackages.${system}.extend rust-overlay.overlays.default;
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           targets = [ "wasm32-unknown-unknown" ];
         };
@@ -56,11 +64,14 @@
         };
 
         # Build demo
-        src = with pkgs;
+        src =
+          with pkgs;
           lib.cleanSourceWith {
             src = ./.;
-            filter = path: type:
-              (lib.hasSuffix ".html" path) || (lib.hasInfix "/assets/" path)
+            filter =
+              path: type:
+              (lib.hasSuffix ".html" path)
+              || (lib.hasInfix "/assets/" path)
               || (craneLib.filterCargoSources path type);
           };
 
@@ -72,53 +83,66 @@
           CARGO_PROFILE = "release";
         };
 
-        cargoArtifacts =
-          craneLib.buildDepsOnly (commonArgs // { doCheck = false; });
+        cargoArtifacts = craneLib.buildDepsOnly (commonArgs // { doCheck = false; });
 
-        demo = craneLib.buildTrunkPackage (commonArgs // {
-          inherit cargoArtifacts;
-          pname = "chartistry-demo";
-          version = "0.0.1";
-          strictDeps = true;
-          wasm-bindgen-cli = wasm-bindgen-cli-local;
+        demo = craneLib.buildTrunkPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            pname = "chartistry-demo";
+            version = "0.0.1";
+            strictDeps = true;
+            wasm-bindgen-cli = wasm-bindgen-cli-local;
 
-          cargoExtraArgs = "--package=demo";
-          trunkExtraBuildArgs = "--public-url /leptos-chartistry";
-          trunkIndexPath = "demo/index.html";
-          # Create symlinks for each of our pages. Enables a static site.
-          postInstall = ''
-            ln -s index.html $out/examples.html
-            mkdir -p $out/examples
-            for f in demo/src/examples/*.rs; do
-              f=''${f##*/} # Remove dir prefix
-              f=''${f%.rs} # Remove file suffix
-              f=''${f//_/-} # Replace underscores with dashes
-              ln -s ../index.html $out/examples/$f.html
-            done
-          '';
-        });
-      in {
+            cargoExtraArgs = "--package=demo";
+            trunkExtraBuildArgs = "--public-url /leptos-chartistry";
+            trunkIndexPath = "demo/index.html";
+            # Create symlinks for each of our pages. Enables a static site.
+            postInstall = ''
+              ln -s index.html $out/examples.html
+              mkdir -p $out/examples
+              for f in demo/src/examples/*.rs; do
+                f=''${f##*/} # Remove dir prefix
+                f=''${f%.rs} # Remove file suffix
+                f=''${f//_/-} # Replace underscores with dashes
+                ln -s ../index.html $out/examples/$f.html
+              done
+            '';
+          }
+        );
+      in
+      {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ trunk-local wasm-bindgen-cli-local ];
+          packages = with pkgs; [
+            trunk-local
+            wasm-bindgen-cli-local
+          ];
         };
 
         checks = {
           inherit demo;
 
-          clippy = craneLib.cargoClippy (commonArgs // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
+          clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
 
-          doc = craneLib.cargoDoc (commonArgs // {
-            inherit cargoArtifacts;
-            cargoDocExtraArgs = "--workspace";
-          });
+          doc = craneLib.cargoDoc (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoDocExtraArgs = "--workspace";
+            }
+          );
 
           audit = craneLib.cargoAudit { inherit src advisory-db; };
           fmt = craneLib.cargoFmt commonArgs;
         };
 
         packages.demo = demo;
-      });
+      }
+    );
 }
